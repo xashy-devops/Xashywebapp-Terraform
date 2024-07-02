8b41f21990516afd9911a6812cbcb08b71bc2a3b
gmaiwun <gmaiwun@gmail.com>
2024-07-01 20:59:19 -0500

    Created the s3 bucket module files for project Phido with defaults

--- var.tf
+++ var.tf
@@ -1,10 +1,11 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
+  default = "xashybucket155451215454545"
 }
 
 variable "tags" {
   description = "A mapping of tags to apply to the S3 bucket"
   type        = map(string)
-  default     = {}
+  default     = {Name = "xashyBucket"}
 }
